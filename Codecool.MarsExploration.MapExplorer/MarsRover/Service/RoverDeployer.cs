bfb55d56fe50b81aee690d2ca61873719da3228a
Citrom333 <citrom333@gmail.com>
2023-02-27 14:16:17 +0100

    Implement interface

---
+++ Codecool.MarsExploration.MapExplorer/MarsRover/Service/RoverDeployer.cs
@@ -0,0 +1,30 @@
+using Codecool.MarsExploration.MapExplorer.Configuration.Model;
+using Codecool.MarsExploration.MapExplorer.MarsRover.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Service;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Service.Placer;
+
+namespace Codecool.MarsExploration.MapExplorer.MarsRover.Service;
+
+public class RoverDeployer:IRoverDeployer
+{
+    private ConfigurationModel _configuration;
+    private ICoordinateCalculator _coordinateCalculator;
+
+    public RoverDeployer(ConfigurationModel configuration, ICoordinateCalculator coordinateCalculator)
+    {
+        _configuration = configuration;
+        _coordinateCalculator = coordinateCalculator;
+    }
+
+    public MarsRoverModel DeployMarsRover(int numberOfRoversDeployed, Map map)
+    {
+        var possibleLandingCoordinates = _coordinateCalculator.GetAdjacentCoordinates(_configuration.LandingSpot, map.Dimension, 1);
+        var landingCoordinate = possibleLandingCoordinates.First(coord => map.Representation[coord.X, coord.Y] == null);
+        var numberOfRover = numberOfRoversDeployed+1;
+        var foundResources = new HashSet<(string, Coordinate)>();
+        return new MarsRoverModel($"rover-{numberOfRover}", landingCoordinate, 4, foundResources);
+    }
+
+}
