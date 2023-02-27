b2af7c68a6d66a33efc208b51f0d406d9b086a22
Citrom333 <citrom333@gmail.com>
2023-02-27 19:16:11 +0100

    Make StepNumber and Outcome able to be set

--- Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
@@ -5,4 +5,10 @@ using Codecool.MarsExploration.MapGenerator.MapElements.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.Simulation.Model;
 
-public record SimulationContext(int StepNumber, int StepsToTimeOut, MarsRoverModel Rover, Coordinate LocationOfSpaceship, Map Map, IEnumerable<string> SymbolsOfPreferredResources, ExplorationOutcome Outcome);
+public record SimulationContext(int StepNumber, int StepsToTimeOut, MarsRoverModel Rover,
+    Coordinate LocationOfSpaceship, Map Map, IEnumerable<string> SymbolsOfPreferredResources,
+    ExplorationOutcome? Outcome)
+{
+    public int StepNumber { get; set; } = StepNumber;
+    public ExplorationOutcome? Outcome { get; set; } = Outcome;
+}
