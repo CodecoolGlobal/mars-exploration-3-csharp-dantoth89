5ac062f81d670db6418574c34cbbeaa2b9aec314
Citrom333 <citrom333@gmail.com>
2023-02-27 18:12:58 +0100

    Change MarsRoverModel properties

--- Codecool.MarsExploration.MapExplorer/MarsRover/Model/MarsRoverModel.cs
+++ Codecool.MarsExploration.MapExplorer/MarsRover/Model/MarsRoverModel.cs
@@ -2,5 +2,14 @@ using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.MarsRover.Model;
 
-public record MarsRoverModel(string Id, Coordinate CurrentPosition, int Sight, HashSet<(string foundResourceSymbol, Coordinate foundResourceCoordinate)> FoundResources);
+public record MarsRoverModel(string Id, Coordinate CurrentPosition, int Sight,
+    HashSet<(string foundResourceSymbol, Coordinate foundResourceCoordinate)> FoundResources)
+{
+    public string Id { get; init; } = Id;
+    public Coordinate CurrentPosition { get; set; } = CurrentPosition;
+    public int Sight { get; init; } = Sight;
+    public HashSet<(string foundResourceSymbol, Coordinate foundResourceCoordinate)> FoundResources { get; set; } =
+        FoundResources;
+
+}
 
