78c29c4d8d6f206970dc85be78e352922ff641a9
lmesi <lszl.emese@gmail.com>
2023-02-27 14:28:42 +0100

    Add ConfigurationValidator methods

---
+++ Codecool.MarsExploration.MapExplorer/Configuration/Service/ConfigurationValidator.cs
@@ -0,0 +1,51 @@
+using Codecool.MarsExploration.MapExplorer.Configuration.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Service;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Configuration.Service;
+
+public class ConfigurationValidator:IConfigurationValidator
+{
+    private readonly CoordinateCalculator _coordinateCalculator;
+    
+    public ConfigurationValidator(CoordinateCalculator coordinateCalculator)
+    {
+        
+        _coordinateCalculator = coordinateCalculator;
+    }
+    
+    public bool Validate(ConfigurationModel configuration, Map map)
+    {
+        return FilePathIsNotEmpty(configuration.MapFilePath) &&
+               LandingSpotIsNotOccupied(configuration.LandingSpot, map) &&
+               ResourcesAreNotEmpty(configuration.NeededResourcesSymbols) &&
+               TimeoutStepsGreaterThanZero(configuration.TimeoutSteps) &&
+               LandingSpotHasFreeAdjacentCoordinate(configuration.LandingSpot, map);
+    }
+    
+    private bool FilePathIsNotEmpty(string filePath)
+    {
+        return filePath != "";
+    }
+    
+    private bool LandingSpotIsNotOccupied(Coordinate landingSpot, Map map)
+    {
+        return map.IsEmpty(landingSpot);
+    }
+    
+    private bool LandingSpotHasFreeAdjacentCoordinate(Coordinate landingSpot, Map map)
+    {
+        return _coordinateCalculator.GetAdjacentCoordinates(landingSpot, 3).Any(coordinate => map.GetByCoordinate(coordinate) is null);
+    }
+    
+    private bool ResourcesAreNotEmpty(IEnumerable<string> resources)
+    {
+        return resources.Any();
+    }
+    
+    private bool TimeoutStepsGreaterThanZero(int timeoutSteps)
+    {
+        return timeoutSteps > 0;
+    }
+}
