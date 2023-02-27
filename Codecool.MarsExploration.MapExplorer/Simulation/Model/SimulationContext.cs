1b07554cb77fa9331e2fed82c314e066e06e84f6
Citrom333 <citrom333@gmail.com>
2023-02-27 13:01:01 +0100

    Add SimulationContext record parameters

--- Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
@@ -1,10 +1,8 @@
+using Codecool.MarsExploration.MapExplorer.Exploration;
+
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
 namespace Codecool.MarsExploration.MapExplorer.Simulation.Model;
 
-public record SimulationContext();
-//int step
-//int stepsToTimeout
-//MarsRover rover
-//Coordinate spaceshipLocation
-//Map map
-//IEnumerable<string> symbolsToLookFor
-//ExplorationOutcome Enum
+public record SimulationContext(int StepNumber, int StepsToTimeOut, MarsRover.Model.MarsRover Rover, Coordinate LocationOfSpaceship, Map Map, IEnumerable<string> SymbolsOfPreferredResources, ExplorationOutcome Outcome);
