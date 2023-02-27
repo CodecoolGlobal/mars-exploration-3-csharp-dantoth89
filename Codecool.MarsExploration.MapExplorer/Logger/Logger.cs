56b12567c59899621c3bd724d5d3074f95b3f642
Daniel Toth <dantoth89@gmail.com>
2023-02-27 16:54:58 +0100

    Add logger

---
+++ Codecool.MarsExploration.MapExplorer/Logger/Logger.cs
@@ -0,0 +1,21 @@
+﻿namespace Codecool.MarsExploration.MapExplorer.Logger;
+
+public class Logger : ILogger
+{
+    private static readonly string WorkDir = AppDomain.CurrentDomain.BaseDirectory;
+ 
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+    
+    public void Log(string message, string logName)
+    {
+        var date = DateTime.Now;
+        var timeForFile = date.ToString().Replace(". ", "-").Replace(":", "-");
+        
+        var filePath = $@"{WorkDir}\Resources\{logName}_{timeForFile}.txt";
+        File.WriteAllText(filePath, message);
+    }
+
+}
