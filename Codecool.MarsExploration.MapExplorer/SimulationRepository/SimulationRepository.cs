3a7826ab59e5f033e8953458b900362441485c0b
bmarci888 <bravikmarci@gmail.com>
2023-02-28 17:30:08 +0100

    Implement simulation repository

--- Codecool.MarsExploration.MapExplorer/SimulationRepository/SimulationRepository.cs
+++ Codecool.MarsExploration.MapExplorer/SimulationRepository/SimulationRepository.cs
@@ -1,6 +1,59 @@
+using Codecool.MarsExploration.MapExplorer.Exploration;
+using Microsoft.Data.Sqlite;
+
 namespace Codecool.MarsExploration.MapExplorer.SimulationRepository;
 
-public class SimulationRepository
+public class SimulationRepository : ISimulationRepository
 {
     
+    private readonly string _dbFilePath;
+
+    public SimulationRepository(string dbFilePath)
+    {
+        _dbFilePath = dbFilePath;
+    }
+    
+    private SqliteConnection GetPhysicalDbConnection()
+    {
+        var dbConnection = new SqliteConnection($"Data Source ={_dbFilePath};Mode=ReadWrite");
+        dbConnection.Open();
+        return dbConnection;
+    }
+    
+    private void ExecuteNonQuery(string query)
+    {
+        using var connection = GetPhysicalDbConnection();
+        using var command = GetCommand(query, connection);
+        command.ExecuteNonQuery();
+    }
+    
+    private static SqliteCommand GetCommand(string query, SqliteConnection connection)
+    {
+        return new SqliteCommand
+        {
+            CommandText = query,
+            Connection = connection,
+        };
+    }
+
+
+    public void Add(int numberOfSteps, int amountOfResources, ExplorationOutcome outcome)
+    {
+        var currentTime = DateTime.Now.ToString();
+        var query = $"INSERT INTO simulations (time_of_the_simulation, number_of_steps, amount_of_resources_found, outcome) VALUES ('{currentTime}','{numberOfSteps}','{amountOfResources}','{outcome.ToString()}')";
+        ExecuteNonQuery(query);
+    }
+
+    public void DeleteAll()
+    {
+        var query = "DELETE FROM simulations";
+        ExecuteNonQuery(query);
+        ResetId();
+    }
+
+    private void ResetId()
+    {
+        var resetIdQuery = "UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='simulations'";
+        ExecuteNonQuery(resetIdQuery); 
+    }
 }
