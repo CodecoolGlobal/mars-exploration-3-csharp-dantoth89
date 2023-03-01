40811e135434c4493d0eb8c04e8fc73765e95100
Daniel Toth <dantoth89@gmail.com>
2023-03-01 17:28:44 +0100

    Add CalculateMapCentral

---
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/Routine/ExploringRoutine.cs
@@ -0,0 +1,19 @@
+﻿namespace Codecool.MarsExploration.MapExplorer.Simulation.Service.Routine;
+using Codecool.MarsExploration.MapExplorer.Simulation.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Service;
+
+public class ExploringRoutine : IExploringRoutine
+{
+    private int _reachedTarget = -1;
+    private ICoordinateCalculator _coordinateCalculator = new CoordinateCalculator();
+    private Coordinate _idealStartingPlace;
+    
+    private Coordinate CalculateMapCentral(SimulationContext simulationContext)
+    {
+        var sizeOfMap = simulationContext.Map.Dimension;
+        var central = (int)sizeOfMap / 2;
+        return new Coordinate(central, central);
+    }
+
+}
