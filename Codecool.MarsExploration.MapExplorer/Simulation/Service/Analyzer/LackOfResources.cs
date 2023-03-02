ff522830acfb8df0134fd8f0153e368656bae57f
lmesi <lszl.emese@gmail.com>
2023-03-02 10:08:21 +0100

    Modify LackOfResources analyzer

--- Codecool.MarsExploration.MapExplorer/Simulation/Service/Analyzer/LackOfResources.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/Analyzer/LackOfResources.cs
@@ -5,14 +5,8 @@ namespace Codecool.MarsExploration.MapExplorer.Simulation.Service.Analyzer;
 
 public class LackOfResources:IAnalyzer
 {
-    private HashSet<Coordinate> _exploredCoordinates;
-    public LackOfResources(HashSet<Coordinate> exploredCoordinates)
-    {
-        _exploredCoordinates = exploredCoordinates;
-    }
-    
     public bool Analyze(SimulationContext simulationContext)
     {
-        return _exploredCoordinates.Count() >= Math.Pow(simulationContext.Map.Dimension, 2) * 0.8;
+        return simulationContext.Rover.DiscoveredCoordinates.Count() >= Math.Pow(simulationContext.Map.Dimension, 2) * 0.8;
     }
 }
