93ce511b84680cc2c06f6cca16965c2a203a20d6
Citrom333 <citrom333@gmail.com>
2023-03-14 17:32:26 +0100

    Add public position property

--- Codecool.MarsExploration.MapExplorer/CommandCenter/Command_Center.cs
+++ Codecool.MarsExploration.MapExplorer/CommandCenter/Command_Center.cs
@@ -14,11 +14,11 @@ public class Command_Center:ICommand_Center
    private List<MarsRoverModel> _rovers;
    private Dictionary<string, int> _deliveredResources;
    private ICoordinateCalculator _coordinateCalculator = new CoordinateCalculator();
-
+public Coordinate Position { get; }
    public Command_Center(int id, Coordinate position, int radius, bool isItActive, List<MarsRoverModel> rovers, Dictionary<string, int> deliveredResources)
    {
       _id = id;
-      _position = position;
+      Position = position;
       _radius = radius;
       _isItActive = isItActive;
       _rovers = rovers;
