9f3fbf537e5baa7fcdf5e78092189931ba06d340
Citrom333 <citrom333@gmail.com>
2023-03-14 18:55:41 +0100

    Add CenterOfCommandCenters.cs

---
+++ Codecool.MarsExploration.MapExplorer/CommandCenter/CommandCenterCordinator/CenterOfCommandCenters.cs
@@ -0,0 +1,6 @@
+namespace Codecool.MarsExploration.MapExplorer.Configuration.CommandCenter.Service.CommandCenterCordinator;
+
+public class CenterOfCommandCenters
+{
+    public List<Command_Center> AllCommandCenters;
+}
