82b48e546bb9cf90b5c14b6ad9951edc828b3d10
Citrom333 <citrom333@gmail.com>
2023-03-14 17:33:41 +0100

    Add methods for command center placing

--- Codecool.MarsExploration.MapExplorer/Simulation/Service/Routine/Exploring/ExplorationSimulatorSteps.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/Routine/Exploring/ExplorationSimulatorSteps.cs
@@ -1,3 +1,4 @@
+using Codecool.MarsExploration.MapExplorer.Configuration.CommandCenter.Service;
 using Codecool.MarsExploration.MapExplorer.Exploration;
 using Codecool.MarsExploration.MapExplorer.Logger;
 using Codecool.MarsExploration.MapExplorer.MarsRover.Model;
@@ -19,9 +20,12 @@ public class ExplorationSimulatorSteps : IExplorationSimulationSteps
     private ILogger _logger;
     private IExploringRoutine _exploringRoutine;
     private ISimulationRepository _simulationRepository;
+    private int _numberOfResources = 0;
+    private int _prevNumberOfResources = 0;
 
     public ExplorationSimulatorSteps(ICoordinateCalculator coordinateCalculator, IAnalyzer successAnalyzer,
-        IAnalyzer timeoutAnalyzer, IAnalyzer lackOfResourcesAnalyzer, ILogger logger, IExploringRoutine exploringRoutine,ISimulationRepository simulationRepository)
+        IAnalyzer timeoutAnalyzer, IAnalyzer lackOfResourcesAnalyzer, ILogger logger,
+        IExploringRoutine exploringRoutine, ISimulationRepository simulationRepository)
     {
         _coordinateCalculator = coordinateCalculator;
         _successAnalyzer = successAnalyzer;
@@ -34,17 +38,20 @@ public class ExplorationSimulatorSteps : IExplorationSimulationSteps
 
     public void Steps(SimulationContext simulationContext)
     {
+
         Movement(simulationContext);
         Scan(simulationContext);
         Analysis(_successAnalyzer, _timeoutAnalyzer, _lackOfResourcesAnalyzer, simulationContext);
         Log(simulationContext);
         IncrementStep(simulationContext);
+        
     }
 
     private void Movement(SimulationContext simulationContext)
     {
-        var possibleCoordinates = _coordinateCalculator.GetEmptyAdjacentCoordinates(simulationContext.Rover.CurrentPosition, simulationContext.Map.Dimension, simulationContext.Map,1 );
-        var nextPlace=_exploringRoutine.ExploreMovement(simulationContext, possibleCoordinates);
+        var possibleCoordinates = _coordinateCalculator.GetEmptyAdjacentCoordinates(
+            simulationContext.Rover.CurrentPosition, simulationContext.Map.Dimension, simulationContext.Map, 1);
+        var nextPlace = _exploringRoutine.ExploreMovement(simulationContext, possibleCoordinates);
         simulationContext.Rover.CurrentPosition = nextPlace;
         simulationContext.VisitedPlaces.Add(nextPlace);
 
@@ -58,6 +65,7 @@ public class ExplorationSimulatorSteps : IExplorationSimulationSteps
             discoveredCoordinates.AddRange(_coordinateCalculator.GetAdjacentCoordinates(
                 simulationContext.Rover.CurrentPosition, simulationContext.Map.Dimension, i));
         }
+
         simulationContext.Rover.DiscoveredCoordinates.UnionWith(discoveredCoordinates);
         simulationContext.Rover.DiscoveredCoordinates.Add(simulationContext.Rover.CurrentPosition);
         foreach (var coordinate in discoveredCoordinates)
@@ -67,11 +75,57 @@ public class ExplorationSimulatorSteps : IExplorationSimulationSteps
             {
                 simulationContext.Rover.FoundResources.Add((
                     simulationContext.Map.Representation[coordinate.X, coordinate.Y]!, coordinate));
+                _numberOfResources = simulationContext.Rover.FoundResources.Count;
             }
         }
     }
 
-    private void Analysis(IAnalyzer successAnalyzer, IAnalyzer timeoutAnalyzer, IAnalyzer lackOfResourcesAnalyzer,
+    private bool NewResourceFound()
+    {
+        var isTrue = _numberOfResources > _prevNumberOfResources;
+        _prevNumberOfResources = _numberOfResources;
+        return isTrue;
+    }
+
+    public IEnumerable<Coordinate> ScanForPlaceForCommandCenter(SimulationContext simulationContext)
+    {
+        var rangeOfMineral = new List<Coordinate>();
+        var rangeOfWater = new List<Coordinate>();
+        foreach (var mineral in 
+                 simulationContext.Rover.FoundResources.Where(res=> res.foundResourceSymbol=="%"))
+        {
+            foreach (var water in 
+                     simulationContext.Rover.FoundResources.Where(res=> res.foundResourceSymbol=="*"))
+            {
+                if (Math.Abs(mineral.foundResourceCoordinate.X - water.foundResourceCoordinate.X) <=
+                    simulationContext.CommandCenterSight * 2 &&
+                    Math.Abs(mineral.foundResourceCoordinate.Y - water.foundResourceCoordinate.Y) <=
+                    simulationContext.CommandCenterSight * 2)
+                {
+                    for (int i = 0; i < simulationContext.CommandCenterSight; i++)
+                    {
+                        rangeOfMineral.AddRange(_coordinateCalculator.GetAdjacentCoordinates(
+                            mineral.foundResourceCoordinate, simulationContext.Map.Dimension, i));
+                    }
+                    for (int i = 0; i < simulationContext.CommandCenterSight; i++)
+                    {
+                        rangeOfWater.AddRange(_coordinateCalculator.GetAdjacentCoordinates(
+                            water.foundResourceCoordinate, simulationContext.Map.Dimension, i));
+                    }
+                }
+            }
+        }
+        return rangeOfMineral.Intersect(rangeOfWater);
+    }
+
+    public bool IsThereAnotherCommandCenter(IEnumerable<Coordinate> possiblePlacesForCMDC, List<Command_Center> commandCenters)
+    {
+        return possiblePlacesForCMDC.Intersect
+            (commandCenters.Select
+                (CMDC => CMDC.Position).ToList()).Count() == 0;
+    }
+
+private void Analysis(IAnalyzer successAnalyzer, IAnalyzer timeoutAnalyzer, IAnalyzer lackOfResourcesAnalyzer,
         SimulationContext simulationContext)
     {
         if (successAnalyzer.Analyze(simulationContext))
