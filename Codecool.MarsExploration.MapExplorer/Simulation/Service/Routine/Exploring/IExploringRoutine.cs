18c1406a7c3c937c1142e53b7dc0f5dbf752822f
Citrom333 <citrom333@gmail.com>
2023-03-15 19:22:25 +0100

    Remove empty lines

--- Codecool.MarsExploration.MapExplorer/Simulation/Service/Routine/Exploring/IExploringRoutine.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/Routine/Exploring/IExploringRoutine.cs
@@ -1,11 +1,8 @@
 using Codecool.MarsExploration.MapExplorer.Simulation.Model;
 using Codecool.MarsExploration.MapGenerator.Calculators.Model;
-
 namespace Codecool.MarsExploration.MapExplorer.Simulation.Service.Routine.Exploring;
 
 public interface IExploringRoutine
 {
-
     Coordinate ExploreMovement(SimulationContext simulationContext, IEnumerable<Coordinate> possiblePlaces);
-
 }
