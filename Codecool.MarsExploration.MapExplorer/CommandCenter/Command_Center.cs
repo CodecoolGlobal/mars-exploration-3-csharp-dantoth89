9781adb97f88b5ecdba7bc813b991371709bd17e
Citrom333 <citrom333@gmail.com>
2023-03-15 16:41:48 +0100

    Add activate method

--- Codecool.MarsExploration.MapExplorer/CommandCenter/Command_Center.cs
+++ Codecool.MarsExploration.MapExplorer/CommandCenter/Command_Center.cs
@@ -12,20 +12,33 @@ public class Command_Center:ICommand_Center
    public string Symbol { get; } = "@";
    public int Radius { get; }
    public bool IsItActive { get; set; } = false;
+   public Coordinate ClosestMineral { get; }
+   public Coordinate ClosestWater { get; }
    public List<MarsRoverModel> Rovers { get; set; }
+   private ConfigurationModel _configuration;
    public Dictionary<string, int> DeliveredResources { get; set; } = new Dictionary<string, int>();
    private ICoordinateCalculator _coordinateCalculator = new CoordinateCalculator();
 public Coordinate Position { get; }
-   public Command_Center(int id, Coordinate position, MarsRoverModel actualRover, ConfigurationModel config)
+   public Command_Center(int id, Coordinate position, MarsRoverModel actualRover, ConfigurationModel config, Dictionary<string, Coordinate> closestResources)
    {
       Id = id;
       Position = position;
       Rovers = new List<MarsRoverModel>() {actualRover};
       Radius = config.CommandCenterSight;
-
+      _configuration = config;
+      ClosestMineral = closestResources["mineral"];
+      ClosestWater = closestResources["water"];
    }
- 
-   
+
+   public void ActivateWhenBuilt()
+   {
+      if (!IsItActive && DeliveredResources["mineral"] == _configuration.CommandCenterCost)
+      {
+         DeliveredResources["mineral"] -= _configuration.CommandCenterCost;
+         IsItActive = true;
+         Console.WriteLine($"building cmdc{Id} is ready at {Position}");
+      }
+   }
    public bool DoWeHaveEnoughMineralForRover()
    {
       
