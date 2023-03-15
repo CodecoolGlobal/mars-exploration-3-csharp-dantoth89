94f0e9c0ae16dba8ce89fa868e6446525a191dd0
Citrom333 <citrom333@gmail.com>
2023-03-15 09:36:44 +0100

    Fix program.cs

--- Codecool.MarsExploration.MapExplorer/Program.cs
+++ Codecool.MarsExploration.MapExplorer/Program.cs
@@ -1,4 +1,6 @@
-﻿using Codecool.MarsExploration.MapExplorer.Configuration.Model;
+﻿using Codecool.MarsExploration.MapExplorer.Configuration.CommandCenter.Service;
+using Codecool.MarsExploration.MapExplorer.Configuration.CommandCenter.Service.CommandCenterCordinator;
+using Codecool.MarsExploration.MapExplorer.Configuration.Model;
 using Codecool.MarsExploration.MapExplorer.Configuration.Service;
 using Codecool.MarsExploration.MapExplorer.Exploration;
 using Codecool.MarsExploration.MapExplorer.Logger;
@@ -18,15 +20,11 @@ namespace Codecool.MarsExploration.MapExplorer;
 class Program
 {
     private static readonly string WorkDir = AppDomain.CurrentDomain.BaseDirectory;
-    private static string _mapFile = $@"{WorkDir}/Resources/exploration-1.map";
-
+    private static char separator = Path.DirectorySeparatorChar;
     private static string _databaseFile =
-        $@"{WorkDir.Replace("/bin/Debug/net6.0/", "")}/Resources/SimulationDataBase.db";
-    
+        $@"{WorkDir.Replace($@"{separator}bin{separator}Debug{separator}net6.0{separator}", "")}{separator}Resources{separator}SimulationDataBase.db";
     private static JSONHandler _jsonHandler = new JSONHandler();
     private static ConfigurationModel _configuration = _jsonHandler.JSONConverter(WorkDir);
-    
-    private static SimulationContext _simulationContext;
     private static IMapLoader _mapLoader = new MapLoader.MapLoader();
     private static ICoordinateCalculator _coordinateCalculator = new CoordinateCalculator();
     private static IRoverDeployer _roverDeployer = new RoverDeployer(_configuration, _coordinateCalculator);
@@ -35,32 +33,24 @@ class Program
     private static IAnalyzer _timeoutAnalyzer = new TimeoutAnalyzer();
     private static IAnalyzer _lackOfResourcesAnalyzer = new LackOfResources();
     private static ILogger _logger = _configuration.LoggerType ? new FileLogger() : new ConsolLogger();
-
     private static ISimulationRepository _simulationRepository =
         new SimulationRepository.SimulationRepository(_databaseFile);
-
     private static IExploringRoutine _exploringRoutine = new ExploringRoutine();
-
+    private static List<Command_Center> _commandCenters = new CenterOfCommandCenters().AllCommandCenters;
     private static IExplorationSimulationSteps _explorationSimulationSteps =
         new ExplorationSimulatorSteps(_coordinateCalculator, _successAnalyzer, _timeoutAnalyzer,
             _lackOfResourcesAnalyzer, _logger, _exploringRoutine, _simulationRepository);
-
     private static IExplorationSimulator _explorationSimulator =
         new ExplorationSimulator(_roverDeployer, _configurationValidator, _explorationSimulationSteps);
-
     private static IReturnSimulator _returnSimulator = new ReturnSimulator(_logger);
-
     public static void Main(string[] args)
     {
-     
         File.Delete($@"{WorkDir}/Resources/message.txt");
         var map = _mapLoader.Load(_configuration.MapFilePath);
-        Console.WriteLine(map);
-         try
-         {
-        var simCont =
-                _explorationSimulator.ExploringSimulator(_mapLoader.Load(_configuration.MapFilePath), _configuration, 1,
-                    _simulationContext);
+        try
+        {
+            var simCont =
+                _explorationSimulator.ExploringSimulator(map, _configuration, 1, _commandCenters);
             if (simCont != null)
             {
                 _returnSimulator.ReturningSimulator(simCont);
@@ -68,20 +58,20 @@ class Program
                 {
                     map.Representation[visited.X, visited.Y] = "0";
                 }
-
-                foreach (var visited in simCont.VisitedForReturn)
-                {
-                    map.Representation[visited.X, visited.Y] = "O";
-                }
+                // foreach (var visited in simCont.VisitedForReturn)
+                // {
+                //     map.Representation[visited.X, visited.Y] = "O";
+                // }
+                Console.WriteLine(_commandCenters.Count);
             }
 
             Console.WriteLine(map);
         }
-         catch (Exception e)
-         {
-             Console.ForegroundColor = ConsoleColor.Red;
-             Console.WriteLine(e.Message);
-             Console.ForegroundColor = ConsoleColor.White;
-         }
+        catch (Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e.Message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 }
