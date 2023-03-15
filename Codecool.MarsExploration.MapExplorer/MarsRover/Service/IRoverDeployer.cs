fee64f633d988a0d77feb7a708ab9b8c11589eee
Citrom333 <citrom333@gmail.com>
2023-03-15 16:45:56 +0100

    Add new parameter for deploying different places

--- Codecool.MarsExploration.MapExplorer/MarsRover/Service/IRoverDeployer.cs
+++ Codecool.MarsExploration.MapExplorer/MarsRover/Service/IRoverDeployer.cs
@@ -1,11 +1,12 @@
 using Codecool.MarsExploration.MapExplorer.Configuration.Model;
 using Codecool.MarsExploration.MapExplorer.MarsRover.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 using Codecool.MarsExploration.MapGenerator.MapElements.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.MarsRover.Service;
 
 public interface IRoverDeployer
 {
-    MarsRoverModel DeployMarsRover(int numberOfRoversDeployed, Map map);
+    MarsRoverModel DeployMarsRover(int numberOfRoversDeployed, Map map, Coordinate landingSpotOfDeployer);
 
 }
