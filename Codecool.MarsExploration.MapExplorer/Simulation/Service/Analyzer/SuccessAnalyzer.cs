8c8e96e88a6f2c58c1aa3288f4753f320c00cd46
Citrom333 <citrom333@gmail.com>
2023-03-16 19:57:28 +0100

    Add all found resources from roverFollower

--- Codecool.MarsExploration.MapExplorer/Simulation/Service/Analyzer/SuccessAnalyzer.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Service/Analyzer/SuccessAnalyzer.cs
@@ -3,14 +3,14 @@ using Codecool.MarsExploration.MapExplorer.Simulation.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.Simulation.Service.Analyzer;
 
-public class SuccessAnalyzer:IAnalyzer
+public class SuccessAnalyzer : IAnalyzer
 {
     public bool Analyze(SimulationContext simulationContext, IRoverFollower roverFollower)
     {
         int minerals = 0;
         int water = 0;
-        
-        foreach (var (foundResourceSymbol, foundResourceCoordinate) in simulationContext.Rover.FoundResources)
+
+        foreach (var (foundResourceSymbol, foundResourceCoordinate) in roverFollower.AllFoundResources())
         {
             if (foundResourceSymbol == "%") minerals++;
             else if (foundResourceSymbol == "*") water++;
